99bc78c66a586f814306e6c1d9876750908d4b85
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-05-04 15:34:04 -0300

    First commit

---
+++ Information-System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Information_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
